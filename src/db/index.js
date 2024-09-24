634f4d7d6a983db4033f03312842bd4d36ed26f3
reachbheru <141500915+reachbheru@users.noreply.github.com>
2024-09-24 18:16:07 +0530

    connect to db

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import DB_NAME from '../constants.js'
+
+const connectDB = (async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGOOSEDB_URL}/${DB_NAME}`);
+        console.log(`connected || host : ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log(`DB connection error : ${error}`);
+        process.exit(1);
+    }
+})()
+
+export default connectDB;
+
+// console.log(`${connectionInstance}`)
