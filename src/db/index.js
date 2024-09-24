8bda875adc86dbfb6bdcf784a275169c3a453a64
reachbheru <141500915+reachbheru@users.noreply.github.com>
2024-09-24 18:17:44 +0530

    connect to db

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import DB_NAME from '../constants.js'
 const connectDB = (async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGOOSEDB_URL}/${DB_NAME}`);
-        console.log(`connected || host : ${connectionInstance.connection.host}`);
+        console.log(`connected || host : ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log(`DB connection error : ${error}`);
         process.exit(1);
@@ -12,5 +12,3 @@ const connectDB = (async () => {
 })()
 
 export default connectDB;
-
-// console.log(`${connectionInstance}`)
