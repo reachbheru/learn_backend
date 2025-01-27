fd7a00cb313e0d08d4c8aa5e23dce14498a3eb59
reachbheru <141500915+reachbheru@users.noreply.github.com>
2025-01-27 18:45:59 +0530

    added new routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import Router from "express";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, changeUserDetails, getCurrentUser, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getUserWatchHistory } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
@@ -23,5 +23,12 @@ router.route("/login").post(loginUser);
 //safe routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").patch(verifyJWT, changeCurrentPassword);
+router.route("/change-details").patch(verifyJWT, changeUserDetails);
+router.route("/get-user").get(verifyJWT, getCurrentUser);
+router.route("/change-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/change-coverimage").patch(verifyJWT, upload.single("coverimage"), updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getUserWatchHistory);
 
 export default router 
